f9864ccb42430c97554ccfe45caf0fa9e3074574
Yuut <ingride.reis.b@gmail.com>
2022-08-15 18:15:35 -0300

    :sparkles: feat: Riscando tarefas concluidas com um click duplo

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const inputTask = document.querySelector('.inputNewTask');
 const btnTask = document.querySelector('.btnNewTask');
 const list = document.querySelector('.list');
 
+
+
+const doneTask = ({target}) => {
+target.classList.toggle('completed')
+}
+    
+ 
 const addList = () => {
    const task = document.createElement('li');
    task.innerText = inputTask.value;
+   task.addEventListener('dblclick', doneTask)
    list.appendChild(task);
    inputTask.value = '' 
 };
+
 
 btnTask.addEventListener('click', addList);
