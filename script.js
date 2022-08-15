3ef6de7bcf8b8b587958ace62d2bcda78a5509ab
Yuut <ingride.reis.b@gmail.com>
2022-08-15 19:03:47 -0300

    :sparkles: feat: Botão para deletar tarefa

--- script.js
+++ script.js
@@ -8,14 +8,24 @@ const doneTask = ({target}) => {
 target.classList.toggle('completed')
 }
     
- 
+const deletTask = ({target}, btn) => { 
+    target.parentNode.remove();
+  } 
+
+
 const addList = () => {
-   const task = document.createElement('li');
-   task.innerText = inputTask.value;
+  const delet = document.createElement('button');
+    delet.innerText = 'delet';
+    const task = document.createElement('li');
+    task.innerText = inputTask.value;
+    task.appendChild(delet)
+    delet.addEventListener('click', deletTask)
    task.addEventListener('dblclick', doneTask)
    list.appendChild(task);
    inputTask.value = '' 
 };
+
+
 
 
 btnTask.addEventListener('click', addList);
