782bc5904e2b5570695d2999160ab5f38fb98f32
Yuut <ingride.reis.b@gmail.com>
2022-08-19 17:35:57 -0300

    :sparkles: feat: drag and drop funcionando

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-let dragged = null;
 const inputTask = document.querySelector('.inputNewTask');
 const btnTask = document.querySelector('.btnNewTask');
 const list = document.querySelector('.dropzone');
 let storage = [];
 
-document.addEventListener('dragstart', ({target}) => {dragged = target})
-document.addEventListener('dragover', (event) => {event.preventDefault()})
-document.addEventListener('drop', (event) => {
-  event.preventDefault()
-  if(event.target.className === 'dropzone') {
-    dragged.parentNode.removeChild(dragged);
-    event.target.appendChild(dragged);
+// Responsavel por capturar a posição do elemento que está sendo arrastado.
+const newposition = (ol, posY) => {
+  const tasknoSelect = ol.querySelectorAll('.task:not(.dragging)') // Todas as li que não estão sendo movimentadas
+  let result; // posição do mouse
+  for (let taskli of tasknoSelect){   // percorrendo os elementos não arrastados
+    const li = taskli.getBoundingClientRect(); // ṕegando o tamanho da li e sua posição Y 
+    const liCenterY = li.y + li.height / 2; // li.y = posição no eixo y + li.height / 2
+    if(posY >= liCenterY) result = taskli;
+  }
+  setStorage()
+  return result // posição do mouse
+}
+
+// Adicionando e removendo a classe dragging ao arrastar e soltar
+document.addEventListener('dragstart', ({ target }) => { target.classList.add('dragging') });
+document.addEventListener('dragend', ({ target }) => { target.classList.remove('dragging') });
+// Aplicando o evento que permite que a OL seja uma "Area soltavel"
+list.addEventListener('dragover', (event) => {
+  const dragging = document.querySelector('.dragging'); // item que está sendo arrastado
+  const after = newposition(list, event.clientY); // a nova posição que o item arrastado vai assumir
+  if(after) {
+    after.insertAdjacentElement('afterend', dragging) // fazendo o "append do item"
+  }else{
+    list.prepend(dragging); // se estiver fazio ele vai ser o primeiro item da lista 
   }
 })
 
@@ -61,8 +77,8 @@ const addList = (value, nameclass) => {
 const taskonload = () => {
   if (localStorage.tasks) {
     storage = JSON.parse(localStorage.tasks)
-    storage.forEach(({task, done}) => {
-      addList(task, done  ? 'task completed' : 'task');
+    storage.forEach(({ task, done }) => {
+      addList(task, done ? 'task completed' : 'task');
     })
   } else {
     storage = []
